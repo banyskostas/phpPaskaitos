47f6ca015590d6fa845cd21862b0eeb7247cfa05
Kostas <banyskostas@gmail.com>
2017-12-27 11:08:19 +0200

    Duomenu paemimas, lenteles rikiavimas pagal stulpelius

---
+++ 7/2.php
@@ -0,0 +1,173 @@
+<?php
+require_once('data/data.php');
+
+$myData = $data;
+
+$possibleIndexes = [
+    'name' => [
+        'sort' => 0
+    ],
+    'date' => [
+        'sort' => 0
+    ],
+    'price' => [
+        'sort' => 0
+    ],
+    'make' => [
+        'sort' => 0
+    ],
+];
+if (isset($_GET)) {
+    // Calculate time elapsed start
+    $start = microtime(true);
+    foreach ($possibleIndexes as $index => $indexData) {
+        if (array_key_exists($index, $_GET)) {
+            $sort = $_GET[$index];
+
+            $arr = [];
+            foreach ($data as $item) {
+                $arr[$item['id']] = $item[$index];
+            }
+
+            // Sort data by name
+            if (!$sort) {
+                uasort($arr, 'cmp');
+            } else {
+                uasort($arr, 'cmpReverse');
+            }
+
+//                    $data = [0 => ['id' => 'a123'], 1 => ['id' => 'b123'], 2 => ['id' => 'c123']];
+
+//                    $arr = ['c123' => 'batai','b123' => 'striuke','a123' => 'kede'];
+
+            $myData = [];
+            foreach ($arr as $id => $name) {
+                foreach ($data as $item) {
+                    if ($item['id'] == $id) {
+                        $myData[] = $item;
+                        break;
+                    }
+                }
+            }
+            $possibleIndexes[$index]['sort'] = !$sort;
+
+            // Explanation of !$sort
+//            if ($sort === true) {
+//                $sort = false;
+//            } else {
+//                $sort = true;
+//            }
+        }
+    }
+
+    // Calculate time elapsed end
+    $time_elapsed_secs = microtime(true) - $start;
+
+    var_dump($time_elapsed_secs .' sec');
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Baltic Talents</title>
+
+    <!-- Bootstrap -->
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+<body>
+<nav class="navbar navbar-default">
+    <div class="container-fluid">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
+                    data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">Baltic Talents</a>
+        </div>
+
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav">
+                <li class="active"><a href="#">Nuoroada 1</a></li>
+                <li><a href="#">Nuoroada 2</a></li>
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true"
+                       aria-expanded="false">Sub nuoroda <span class="caret"></span></a>
+                    <ul class="dropdown-menu">
+                        <li><a href="#">Sum meniu 1</a></li>
+                        <li><a href="#">Sum meniu 2</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+
+<div class="container container-fluid">
+    <table class="table table-condensed">
+        <thead>
+        <tr>
+            <th><a href="2.php?name=<?php echo strval($possibleIndexes['name']['sort']); ?>">Pavadinimas</a></th>
+            <th><a href="2.php?date=<?php echo strval($possibleIndexes['date']['sort']); ?>">Data</a></th>
+            <th><a href="2.php?price=<?php echo strval($possibleIndexes['price']['sort']); ?>">Kaina</a></th>
+            <th><a href="2.php?make=<?php echo strval($possibleIndexes['make']['sort']); ?>">Pagaminta</a></th>
+        </tr>
+        </thead>
+        <tbody>
+        <?php
+        foreach ($myData as $item) {
+            echo ' <tr>
+                <td>' . $item['name'] . '</td>
+                <td>' . $item['date'] . '</td>
+                <td>' . $item['price'] . '</td>
+                <td>' . $item['make'] . '</td>
+            </tr>';
+        }
+        ?>
+        </tbody>
+    </table>
+</div>
+
+
+</div>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</body>
+</html>
+
+<?php
+function cmp($a, $b)
+{
+    if ($a == $b) {
+        return 0;
+    }
+    return ($a < $b) ? -1 : 1;
+}
+
+function cmpReverse($a, $b)
+{
+    if ($a == $b) {
+        return 0;
+    }
+    return ($a < $b) ? 1 : -1;
+}
+
+?>
+
+
+
---
+++ 7/content.php
@@ -0,0 +1,36 @@
+<?php
+$turinys = array (
+		1 => array (
+				'name' => 'Apie mus',
+				'text' => '<p>Klaipėdos universitetas – tai daugiasritis, į tarptautinius akademinius tinklus integruotas nacionalinis ir Baltijos regiono jūros mokslų ir studijų lyderis, kultūros paveldo puoselėtojas ir mokymosi visą gyvenimą centras.</p>
+							<p>Klaipėdos universitetas įsteigtas 1991 m. sausio 1 d. Savo veiklą jis pradėjo turėdamas tris fakultetus, o 25-ąjį jubiliejų pasitinka išaugęs į 4 fakultetus: Humanitarinių ir ugdymo mokslų, Jūros technologjų ir gamtos mokslų, Socialinių ir Sveikatos mokslų; 1 studijų (Tęstinių studijų) ir 1 mokslo (Baltijos regiono istorijos ir archeologijos) institutus, Menų akademiją. Sparčiai augančiame universitete studijuoja apie 4500 studentų. Šioje Alma Mater studijas yra baigę daugiau nei 32 500 absolventų, kurių gretose yra mokslo daktarai, Seimo ir Vyriausybės nariai, stambių įmonių vadovai, inžinieriai, mokytojai, solistai ir teatralai.</p>',
+				'image' => 'http://alkas.lt/wp-content/uploads/2011/07/klaipedos-universitetas.jpg',
+				'gallery' => array (
+						'http://www.jjc.edu/admissions/SiteAssets/Happy-students1.jpg',
+						'https://cache-blog.credit.com/wp-content/uploads/2013/04/student-loans-ts-1360x860.jpg'
+				)
+		),
+		2 => array (
+				'name' => 'Struktūra',
+				'text' => '<p>Klaipėdos universiteto Taryba yra kolegialus Klaipėdos universiteto valdymo organas. Taryba vadovaujasi Lietuvos Respublikos Mokslo ir studijų įstatymu, Lietuvos Respublikos  teisės aktais, Universiteto statutu ir Tarybos darbo reglamentu. Taryba tvirtina universiteto viziją ir misiją, Rektoriaus pateiktą strateginį veiklos planą, svarsto ir tvirtina Rektoriaus teikiamus universiteto struktūros pertvarkos planus, nustato universiteto Rektoriaus rinkimų viešo konkurso būdu organizavimo tvarką, renka, skiria į pareigas ir atleidžia iš jų Rektorių, nustato universiteto lėšų ir turto valdymo, naudojimo ir disponavimo jais tvarką, svarsto ir tvirtina svarbiausius su tuo susijusius sprendimus, rūpinasi parama universitetui bei atlieka kitas funkcijas. Universiteto Taryba sudaroma 5 metams iš 9 narių.</p>',
+				'image' => 'http://senas.ku.lt/en/wp-content/uploads/2015/08/DSC_1882-770x510.jpg',
+				'gallery' => array (
+						'http://www.jjc.edu/admissions/SiteAssets/Happy-students1.jpg',
+						'https://cache-blog.credit.com/wp-content/uploads/2013/04/student-loans-ts-1360x860.jpg'
+				)
+		),
+);
+
+$news=array(
+		'2016-12-16' => array(
+				'name'=>'Naujienos pavadinimas',
+				'text'=>'Naujienos tekstas ilgas',
+				'description'=>'Naujienos trumpas aprašymas'
+		),
+		'2016-12-14' => array(
+				'name'=>'Naujienos pavadinimas',
+				'text'=>'Naujienos tekstas ilgas',
+				'description'=>'Naujienos trumpas aprašymas'
+		)
+			
+);
